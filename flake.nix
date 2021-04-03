b6ef7025a8c77ad8e32c997e2149af2bf1cc7ec2
Ryan Mulligan <ryan@ryantm.com>
2021-04-02 20:52:24 -0700

    fix website building

--- flake.nix
+++ flake.nix
@@ -2,14 +2,16 @@
   inputs.nixpkgs.url = "nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.mmdoc.url = "/home/ryantm/p/mmdoc";
-  inputs.mmdoc.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = { self, nixpkgs, flake-utils, mmdoc } :
-    flake-utils.lib.eachDefaultSystem (system: {
-      packages."ryantm.com" = nixpkgs.legacyPackages.${system}.callPackage ./pkgs/ryantm.com.nix {
-        inherit self;
-        inherit (mmdoc.packages.${system}) mmdoc;
-      };
-      defaultPackage = self.packages.${system}."ryantm.com";
-    });
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages."ryantm.com" = pkgs.callPackage ./pkgs/ryantm.com.nix {
+          inherit self;
+          inherit (mmdoc.packages.${system}) mmdoc;
+        };
+        defaultPackage = self.packages.${system}."ryantm.com";
+      });
 }
--- pkgs/ryantm.com.nix
+++ pkgs/ryantm.com.nix
@@ -1,11 +1,12 @@
 {stdenv,
 self,
 mmdoc,
+strace
 } :
 
 stdenv.mkDerivation rec {
   name = "ryantm.com";
   src = self;
-  buildInputs = [ mmdoc ];
-  installPhase = "mmdoc ryantm.com $src/src $out";
+  phases = [ "mmdocPhase" ];
+  mmdocPhase = "${mmdoc}/bin/mmdoc ryantm.com $src/src $out";
 }
